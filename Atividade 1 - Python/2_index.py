c1507fd0e40f7915d498eac717f8cb6acb5220f0
CatWolfyy <liviaantenucci@colegiopaulodetarso.com.br>
2025-02-26 16:17:41 +0000

    Atividade 1

---
+++ Atividade 1 - Python/2_index.py
@@ -0,0 +1,7 @@
+def soma_dois_numeros(numero1, numero2):
+  soma = numero1 + numero2
+  return soma
+# Função de soma entre dois números escolhidos pelo usuário.
+numero1 = input("Escolha o primeiro número.")
+numero2 = input("Escolha o segundo número.")
+soma_dois_numeros(numero1, numero2)
