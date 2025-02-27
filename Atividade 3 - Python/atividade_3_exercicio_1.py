8f3a38a3bea4aaf3b17dd09247b024c7659d64a3
CatWolfyy <liviaantenucci@colegiopaulodetarso.com.br>
2025-02-27 23:28:28 +0000

    Atividade 3 Exercício 1 Completo

--- Atividade 3 - Python/atividade_3_exercicio_1.py
+++ Atividade 3 - Python/atividade_3_exercicio_1.py
@@ -1,3 +1,3 @@
-# Pare de Gritar!
-nome = input("Digite seu nome.")
-.lower()
+# Pare de gritar - NO CAPS PLEASE
+resposta = input("Escreva algo em caixa alta. ") # Pede ao usuário uma string.
+print(resposta.lower()) # Converte a string para minúsculo.
