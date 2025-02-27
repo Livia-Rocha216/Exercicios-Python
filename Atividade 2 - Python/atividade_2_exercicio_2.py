5b7d957c8d3513a2505d2512be6e536713391780
CatWolfyy <liviaantenucci@colegiopaulodetarso.com.br>
2025-02-27 21:10:08 +0000

    Atividade 1 e 2 Completas

--- Atividade 2 - Python/atividade_2_exercicio_2.py
+++ Atividade 2 - Python/atividade_2_exercicio_2.py
@@ -1,10 +1,6 @@
 # Can you drive?
-idade = input("Diga sua idade.")
-if idade <= 18:
+idade = float(input("Digite sua idade."))
+if idade < 18: # Se a idade escrita pelo usuário for menor que 18, retorna menor de idade.
   print("Você é menor de idade.")
-elif idade >= 19 or idade <= 59:
-  print("Você é adulto.")
-elif idade >= 60:
-  print("Você é idoso.")
-else: idade <= 0
-print("Isso não é um número válido.")
+elif idade >= 18: # Se a idade escrita pelo usuário for maior que 18, retorna maior de idade.
+  print("Você é maior de idade.")
