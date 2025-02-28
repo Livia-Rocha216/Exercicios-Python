474409702953d51c2bc7efaff54273827f456b07
CatWolfyy <liviaantenucci@colegiopaulodetarso.com.br>
2025-02-28 02:01:28 +0000

    Atividade 3 Exercícios 1 Completo e 2 Incompleto

---
+++ Atividade 3 - Python/atividade_3_exercicio_2.py
@@ -0,0 +1,3 @@
+# Fale devagar
+resposta = input("Escreva uma frase.") # Pede uma string ao usuário.
+print(resposta.split()) # Divide a string, com o carácter selecionado. (não está funcionando, só o carácter padrão :c)
